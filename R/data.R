cbe52ca7d46122798b1ddffde8a9ebdc1fd6fefa
meriem-t <53788149+meriem-t@users.noreply.github.com>
2026-01-07 22:43:57 -0500

    Update simulated data documentation

--- R/data.R
+++ R/data.R
@@ -2,15 +2,13 @@
 #'
 #' A simulated dataset for demonstrating the Bayesian Consensus Factor Model.
 #'
-#' @format A data frame with 200 rows and 20 variables:
-#' \describe{
-#'   \item{Var1-Var20}{Simulated numeric variables}
-#' }
+#' @format A data frame with 200 rows and 20 variables (V1 through V20):
+#' All variables are simulated numeric values.
 #' 
 #' @source Simulated data generated for package examples
 #' 
 #' @examples
 #' data(sim.data)
-#' dim(sim.data)  # 200 x 20
+#' dim(sim.data)
 #' head(sim.data)
 "sim.data"
